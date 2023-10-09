c130dc7c8544f8008ecff8e67711aec7a615dcc1
2Manuel-2 <manuel.miranda0285@gmail.com>
2023-10-08 20:30:20 -0600

    update

---
+++ src/main/java/sv/edu/udb/www/managedbeans/EmpresaBean.java
@@ -0,0 +1,20 @@
+package sv.edu.udb.www.managedbeans;
+import jakarta.faces.bean.ManagedBean;
+import jakarta.faces.bean.RequestScoped;
+import sv.edu.udb.www.entities.EmpresasEntity;
+import sv.edu.udb.www.models.EmpresasModel;
+import sv.edu.udb.www.utils.JsfUtil;
+import java.util.List;
+@ManagedBean
+@RequestScoped
+public class EmpresaBean {
+
+    EmpresasModel modelo = new EmpresasModel();
+    private List<EmpresasEntity> listaEmpresas;
+
+    public List<EmpresasEntity> getListaEmpresas() {
+        // Llama al método en el modelo para obtener la lista de empresas
+        // Por ejemplo, podrías usar EmpresaModel.listarEmpresas()
+        return modelo.listarEmpresas();
+    }
+}
---
+++ src/main/java/sv/edu/udb/www/models/EmpresasModel.java
@@ -0,0 +1,25 @@
+package sv.edu.udb.www.models;
+import sv.edu.udb.www.utils.JpaUtil;
+import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+import sv.edu.udb.www.entities.EmpresasEntity;
+
+public class EmpresasModel {
+    public List<EmpresasEntity> listarEmpresas() {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            // Utiliza una consulta JPQL para obtener la lista de empresas
+            Query consulta = em.createQuery("SELECT e FROM EmpresasEntity e");
+            List<EmpresasEntity> listaEmpresas = consulta.getResultList();
+            return listaEmpresas;
+        } catch (Exception e) {
+            // Maneja cualquier excepción que pueda ocurrir durante la consulta
+            e.printStackTrace();
+            return null;
+        } finally {
+            em.close(); // Cierra el EntityManager cuando hayas terminado
+        }
+    }
+}
