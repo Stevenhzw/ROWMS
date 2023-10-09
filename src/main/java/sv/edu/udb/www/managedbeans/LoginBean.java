831969cde3ca307dfcdef03bf80cbd29e3eb229c
Stevenhzw <100164210+Stevenhzw@users.noreply.github.com>
2023-10-08 22:49:24 -0600

    bueno creo

--- src/main/java/sv/edu/udb/www/managedbeans/LoginBean.java
+++ src/main/java/sv/edu/udb/www/managedbeans/LoginBean.java
@@ -3,9 +3,8 @@ package sv.edu.udb.www.managedbeans;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import sv.edu.udb.www.entities.UsuariosEntity;
+import sv.edu.udb.www.utils.JpaUtil;
 
 @Named
 @RequestScoped
@@ -14,9 +13,7 @@ public class LoginBean {
     private String contraseña;
 
     public String iniciarSesion() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
-        EntityManager em = emf.createEntityManager();
-
+        EntityManager em = JpaUtil.getEntityManager();
         UsuariosEntity usuario = em.createNamedQuery("UsuariosEntity.findByCorreo", UsuariosEntity.class)
                 .setParameter("correo", correo)
                 .getSingleResult();
--- src/main/java/sv/edu/udb/www/utils/JpaUtil.java
+++ src/main/java/sv/edu/udb/www/utils/JpaUtil.java
@@ -11,6 +11,3 @@ public class JpaUtil {
         return emFactory.createEntityManager();
     }
 }
-
-
-
