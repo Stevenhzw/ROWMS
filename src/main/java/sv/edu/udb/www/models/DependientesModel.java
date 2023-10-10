504d34cd8aba451c371da7b401b9786286c6c27e
2Manuel-2 <manuel.miranda0285@gmail.com>
2023-10-09 20:33:54 -0600

    update sentencia

--- src/main/java/sv/edu/udb/www/models/DependientesModel.java
+++ src/main/java/sv/edu/udb/www/models/DependientesModel.java
@@ -38,6 +38,7 @@ public class DependientesModel {
             Query consulta = em.createQuery("SELECT e FROM UsuariosEntity e " +
                     "WHERE LOWER(e.nombre) LIKE :filtro " +
                     "OR LOWER(e.dui) LIKE :filtro " +
+                    "OR LOWER(e.correo) LIKE :filtro " +
                     "OR LOWER(e.apellido) LIKE :filtro " +
                     "OR LOWER(e.direccion) LIKE :filtro");
             consulta.setParameter("filtro", "%" + filtro.toLowerCase() + "%");
